a982cb6d17b15239d0fb4ab6889525535c97ba22
omar farage ibrahim khalil <omrsamir01003@gmail.com>
2023-10-19 23:50:38 +0300

    initial project. Added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/home_screen.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
